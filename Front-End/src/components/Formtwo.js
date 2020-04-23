f22e9cf53474e93e2b05e6a597efb4fea0942a40
zaiba19 <zaibaiqbal1@gmail.com>
2020-04-23 00:44:08 -0400

    was able to get lat and lon of user but need to fix bugs

--- Front-End/src/components/Formtwo.js
+++ Front-End/src/components/Formtwo.js
@@ -4,9 +4,9 @@ export class Formtwo extends Component {
     render() {
         return (
            <form onSubmit={this.props.getWeather}>
-               <input className="inputt" type="text" name="city" placeholder="City..."></input>
-               <input className="inputt" type="text" name="country" placeholder="Country..."></input>
-               <button>Get Weather</button>
+               <input className="inputt" type="text" name="latitude" placeholder="Lat..."></input>
+               <input className="inputt" type="text" name="longitude" placeholder="Lon..."></input>
+               <button>Get Weathers</button>
            </form>
         )
     }
