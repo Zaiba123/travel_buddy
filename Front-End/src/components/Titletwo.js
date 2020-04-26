80a98ffc2a338db40cb8db07e82a21d02d2092af
zaiba19 <zaibaiqbal1@gmail.com>
2020-04-26 16:36:06 -0400

    fixed longitude bug

---
+++ Front-End/src/components/Titletwo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Titletwo extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title-container__title">Weather Finder</h1>
+                <p className="title-container__subtitle">Find out weather forecast...</p>
+            </div>
+        )
+    }
+}
+
+export default Titletwo
