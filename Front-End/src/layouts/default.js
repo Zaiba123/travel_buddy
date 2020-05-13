02619abd5b7b3f9e63edcd43fbbc847b95f38b49
Adrian Barros <adriansbarros1@gmail.com>
2020-05-13 17:47:52 -0400

    navbar update

--- Front-End/src/layouts/default.js
+++ Front-End/src/layouts/default.js
@@ -1,24 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export class DefaultLayout extends Component {
   state = {
-  
+
   };
-  
+
   render() {
     return (
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand as={Link} to="/">Location App</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Eventurous</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/contact-us">Full Package</Nav.Link>
             <Nav.Link as={Link} to="/currency-exchange">Currency Exchange</Nav.Link>
             <Nav.Link as={Link} to="/about-us">About Us</Nav.Link>
-            <Nav.Link as={Link} to="/contact-us">Forecast</Nav.Link>
-            <Nav.Link as={Link} to="/hub-page">Hub Page</Nav.Link>
-
           </Nav>
         </Navbar.Collapse>
       </Navbar>
