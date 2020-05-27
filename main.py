7908092b3f3d4f9b48c9026ad90cecab90fd3557
Adrian Barros <adriansbarros1@gmail.com>
2020-05-27 17:16:26 -0400

    python index

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ PREDICT_HQ_API_SECRET = os.getenv('PREDICT_HQ_API_S')
 PREDICT_HQ_API_ACCESS_TOKEN = os.getenv('PREDICT_HQ_API_ACCESS')
 WEATHER_STACK_API_KEY = os.getenv('WEATHER_STACK_API')
 
-app = Flask(__name__, static_folder="../build", static_url_path="/")
+app = Flask(__name__, static_folder="..Front-End/build", static_url_path="/")
 
 CORS(app)
 @app.route("/")
